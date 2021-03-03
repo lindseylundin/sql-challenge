74699be2b1a0587bf46a250a9a0ff0f39103f2c6
lindseylundin <lundin.lindsey@gmail.com>
2021-03-03 01:11:18 -0500

    updating homework

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -1,7 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
+﻿-- Create Table
 CREATE TABLE "departments" (
     "dept_no" VARCHAR   NOT NULL,
     "dept_name" VARCHAR   NOT NULL,
@@ -10,14 +7,17 @@ CREATE TABLE "departments" (
      )
 );
 
+
+-- Create Table
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
     "dept_no" VARCHAR   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no"
+        "emp_no", "dept_no"
      )
 );
 
+-- Create Table
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR   NOT NULL,
     "emp_no" INT   NOT NULL,
@@ -26,19 +26,22 @@ CREATE TABLE "dept_manager" (
      )
 );
 
+
+-- Create Table
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" VARCHAR   NOT NULL,
-    "birth_date" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
     "first_name" VARCHAR   NOT NULL,
     "last_name" VARCHAR   NOT NULL,
     "sex" VARCHAR   NOT NULL,
-    "hire_date" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
+-- Create Table
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
     "salary" INT   NOT NULL,
@@ -47,6 +50,8 @@ CREATE TABLE "salaries" (
      )
 );
 
+
+-- Create Table
 CREATE TABLE "titles" (
     "title_id" VARCHAR   NOT NULL,
     "title" VARCHAR   NOT NULL,
@@ -55,6 +60,9 @@ CREATE TABLE "titles" (
      )
 );
 
+-- Import CSV files to corresponding tables
+
+-- Add Foreign Keys
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
@@ -67,9 +75,19 @@ REFERENCES "departments" ("dept_no");
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
 
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+
+-- Check to make sure CSV data imported to table 
+SELECT * FROM departments
+SELECT * FROM dept_emp
+SELECT * FROM dept_manager
+SELECT * FROM employees
+SELECT * FROM salaries
+SELECT * FROM titles
+;
 
